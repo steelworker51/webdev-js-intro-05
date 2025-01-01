1118ec88d5dd9b9ea51d5cea383ec0edf24a5fbd
dale murphy <smcpsycho51@gmail.com>
2025-01-01 16:30:26 -0500

    changes

--- js/assignment.js
+++ js/assignment.js
@@ -10,6 +10,9 @@ const todos = [
 
 // Declare your two immutable variables
 
+let shiftValue;
+let popValue;
+
 
 // Declaring constant variables to store references to the elements
 // that will be updated by your code.
@@ -22,15 +25,28 @@ const answerSixEl = document.getElementById("answer-six");
 const submissionBtn = document.getElementById("submission-btn");
 
 function updateAnswerOne() {
-    
+    console.log(todos)
 }
 
 function updateAnswerTwo() {
-
+    console.log(todos[2])
 }
 
 function removeLastValue() {
-
+    const todos = [
+        "Wash the dishes",
+        "Clean the gutters",
+        "Mow the yard",
+        "Do some coding",
+        "Rake the leaves",
+    ];
+    
+    
+    let popValue = todos.pop();
+    
+    console.log(popValue);
+    console.log(todos);
+    
 }
 
 function removeFirstValue() {
--- js/index.js
+++ js/index.js
@@ -1,3 +1,41 @@
 // Prevent us from attempting to use variables
 // that are not declared
 "use strict"
+
+//let dailyTasks = ["Work", "Gym", "Housework", "Kids/Family time", "Sleep"]
+
+//console.log(dailyTasks);
+
+/*let favThings = ["ScaryMovies", "Gym Time", 19, "Black", "Music"]
+//it will show strings as well as numbers
+
+console.log(favThings);
+
+let favThings = ["ScaryMovies", "Gym Time", 19, "Black", "Music"];
+console.log(typeof favThings); // Output: "object"
+
+console.log(Array.isArray(favThings)); // Output: true */
+
+/*let dailyTasks = ["Work", "Gym", "Housework", "Kids/Family time", "Sleep"]
+
+dailyTasks.shift();
+
+console.log(dailyTasks);*/
+
+/*let dailyTasks = ["Work", "Gym", "Housework", "Kids/Family time", "Sleep"]
+
+dailyTasks.push("Take out the trash");
+
+console.log(dailyTasks);*/
+
+/*let favThings = ["ScaryMovies", "Gym Time", 19, "Black", "Music"];
+
+
+favThings[0] = "ActionMovies";   
+favThings[1] = "Reading Time";     
+favThings[2] = 25;               
+favThings[3] = "Red";            
+favThings[4] = "Television Shows";       
+
+console.log(favThings);*/ 
+
